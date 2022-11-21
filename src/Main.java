26c2fa881891428ace60fad5e6a59ee61bf9d021
FNechepal <ithysbit@bitbucket.org>
2022-11-21 20:08:45 +0300

    add arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите п1ервое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number1 + number2;
+        int minus  = number1 - number2;
+        int multuplite = number1 * number2;
+        double quotient = (double) number1 / number2;
+        System.out.println("Сумма:" + sum);
+        System.out.println("Разность:" + minus);
+        System.out.println("Произведение:" + multuplite);
+        System.out.println("Частное:" + quotient);
     }
 }
