544baff48ac1caa971a0c01532ed8422f1faed1a
FNechepal <ithysbit@bitbucket.org>
2022-12-03 17:34:31 +0300

    add logic found file or not

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,20 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+        int count = 1;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if ((fileExist || isDirectory) == false) {
+                System.out.println("Файл не существует или указанный путь является путём к папке");
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count++);
+            }
+        }
     }
 }
