e66f7b79a72dc0717d61ac3a141f792bffbbb8b8
FNechepal <ithysbit@bitbucket.org>
2022-12-03 17:32:21 +0300

    add logic found file or not

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите п1ервое число:");
+        System.out.println("Введите первое число:");
         int number1 = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int number2 = new Scanner(System.in).nextInt();
@@ -14,5 +15,18 @@ public class Main {
         System.out.println("Разность:" + minus);
         System.out.println("Произведение:" + multuplite);
         System.out.println("Частное:" + quotient);
+
+        int count=1;
+        while(true){
+         String path = new Scanner (System.in).nextLine();
+         File file = new File(path);
+         boolean fileExist = file.exists();
+         boolean isDirectory = file.isDirectory();
+         if ((fileExist || isDirectory)==false)
+         {System.out.println("Файл не существует или указанный путь является путём к папке");
+         continue;}
+         else {System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count++);}
+        }
     }
 }
